2476c20dbe0b4f4fc4e3dd6572e543b78ae7524d
Piyush Gupta <PIYUSHGUPTA53@GMAIL.COM>
2024-05-25 18:34:20 +0530

    do otp verification and unique username check

---
+++ src/app/api/check-unique-username/route.ts
@@ -0,0 +1,72 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { z } from "zod";
+import { usernameValidation } from "@/schemas/signUpSchema";
+
+const UsernameQuerySchema = z.object({
+  username: usernameValidation,
+});
+
+export async function GET(request: Request) {
+  await dbConnect();
+
+  try {
+    const { searchParams } = new URL(request.url);
+
+    const queryParam = {
+      username: searchParams.get("username"),
+    };
+
+    const parsedUsername = UsernameQuerySchema.safeParse(queryParam);
+
+    if (!parsedUsername.success) {
+      const usernameErrors =
+        parsedUsername.error.format().username?._errors || [];
+
+      return Response.json(
+        {
+          success: false,
+          message:
+            usernameErrors.length > 0
+              ? usernameErrors.join(", ")
+              : "Invalid username",
+        },
+        { status: 400 }
+      );
+    }
+
+    const { username } = parsedUsername.data;
+
+    const existingVerifiedUser = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingVerifiedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username already exists",
+        },
+        { status: 400 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "Username is available",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error checking unique username: ", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error checking unique username",
+      },
+      { status: 500 }
+    );
+  }
+}
